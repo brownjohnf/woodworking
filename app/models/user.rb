e17b92b2d5ff55609f4e2dde768479c6a1d93dcd
John Brown <jack@brownjohnf.com>
2012-02-11 16:59:52 +0000

    added support for changing user permissions

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :role_ids
   
   has_and_belongs_to_many :roles
   
+  accepts_nested_attributes_for :roles, :allow_destroy => true
   validates :name, :email, :presence => true
   
   after_create :add_user_role
